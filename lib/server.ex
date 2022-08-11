a5de650ac2b6accb4a6a7c3b687b9bd8648d12a9
Peter Achieng <peterachieng4@gmail.com>
2022-08-11 16:40:53 +0300

    genserver

---
+++ lib/server.ex
@@ -0,0 +1,29 @@
+defmodule Server do
+  use GenServer
+
+  # client side
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def get_msgs(pid) do
+    GenServer.call(pid, :get_msgs)
+  end
+
+  def add_msg(pid, msg) do
+    GenServer.cast(pid, {:add_msg, msg})
+  end
+
+  # server side
+  def init(msgs) do
+    {:ok, msgs}
+  end
+
+  def handle_call(:get_msgs, _from, msgs) do
+    {:reply, msgs, msgs}
+  end
+
+  def handle_cast({:add_msg, msg}, msgs) do
+    {:noreply, [msg | msgs]}
+  end
+end
